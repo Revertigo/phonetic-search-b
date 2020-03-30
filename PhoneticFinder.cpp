fb5aa30da4c3541e5dcb8fc31a66a75b893d638f
dekel241 <dekel241@gmail.com>
2020-03-30 03:39:52 +0300

    Implement new method

--- PhoneticFinder.cpp
+++ PhoneticFinder.cpp
@@ -12,8 +12,10 @@ using namespace std;
 
 namespace phonetic
 {
-    string find(string text, string word)
+    //Initialize vector with all possible chars mistake exchange. Yea, call by reference... whatttt ?!?! what ?!?!?
+    static inline void init_vectors(vector<vector<char>>  & letters)
     {
+        //I have no choice but to inline it or otherwise all vectors will be popout of stack at the end of the function.
         vector<char> exchange_vw {'v', 'w'};
         vector<char> exchange_pbf {'p', 'b', 'f'};
         vector<char> exchange_gj {'g', 'j'};
@@ -22,8 +24,7 @@ namespace phonetic
         vector<char> exchange_dt {'d', 't'};
         vector<char> exchange_ou {'o', 'u'};
         vector<char> exchange_iy {'i', 'y'};
-        
-        vector<vector<char>> letters(ABC_SIZE);//For all abc letters
+
         letters.insert(letters.begin() + 'v' - ALIGN_ASCII, exchange_vw);
         letters.insert(letters.begin() + 'w' - ALIGN_ASCII, exchange_vw);
 
@@ -49,12 +50,16 @@ namespace phonetic
 
         letters.insert(letters.begin() + 'i' - ALIGN_ASCII, exchange_iy);
         letters.insert(letters.begin() + 'y' - ALIGN_ASCII, exchange_iy);
-
+    }
+    
+    string find(string text, string word)
+    {
+        vector<vector<char>> letters(ABC_SIZE);//For all abc letters
+        init_vectors(letters);
         istringstream iss(text);//Initialize streamstring like split
         string token;
         string target = "";
 
-        float max_match_percent = 0.0;
         //Lower case the word to search
         transform(word.begin(), word.end(), word.begin(), ::tolower);
         while(iss >> token)
@@ -62,36 +67,62 @@ namespace phonetic
             //Strings needs to be in the same length in order to be candidate
             if(token.length() == word.length())
             {
-                float temp_match_percent = 0.0;
-                int count_match = 0;
-                string lc_token(token);//Because we don't want to destroy the original token
-                //lower case token string
-                transform(lc_token.begin(), lc_token.end(), lc_token.begin(), ::tolower);
-                for(int i = 0; i < lc_token.length(); i++)
+                //Because we don't want to destroy the original word lower cased word
+                string temp_word(word);
+
+                int counter_match_letters = 0;
+                //Check wether the word could have bin the searched token
+                for(int i = 0; i < temp_word.length(); i++)
                 {
-                    if(lc_token.at(i) == word.at(i))
+                    char curr_ch = temp_word.at(i);
+                    //Check if there is a potential wrong character
+                    if(curr_ch != token.at(i))
                     {
-                        count_match++;
+                        auto exchange_vec = letters.at(curr_ch);
+                        bool found_char = false;
+                        if(!exchange_vec.empty())
+                        {
+                            for(int j = 0; j < exchange_vec.size(); j++)
+                            {
+                                if(exchange_vec.at(j) == token.at(i))
+                                {
+                                    found_char = true;
+                                    counter_match_letters++;
+                                    break;
+                                }
+                            }
+                        }
+
+                        //If it's true, that means this is not the word we are searching for
+                        if(!found_char)
+                        {
+                            break;
+                        }
+                    }
+                    else//If they are equals, moves to the next letter
+                    {
+                        counter_match_letters++;
                     }
                 }
 
-                temp_match_percent = ((float)((count_match)) / lc_token.length()) * 100.0;
-                if(temp_match_percent > max_match_percent)
+
+                if(counter_match_letters == token.length())
                 {
+                    //We have a match !
                     target = token;
-                    max_match_percent = temp_match_percent;
                 }
+
             }
         }
 
-        // if(max_match_percent < THRESHOLD)
-        // {
-        //     //Let's format an output message !
-        //     ostringstream oss;
-        //     oss << "No such word " << word << endl;
-        //     string output = oss.str();
-        //     throw invalid_argument(output);
-        // }
+        if(target == "")
+        {
+            //Let's format an output message !
+            ostringstream oss;
+            oss << "No such word " << word << endl;
+            string output = oss.str();
+            throw invalid_argument(output);
+        }
 
         return target;
     }
